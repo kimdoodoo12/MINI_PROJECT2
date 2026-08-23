cea2e11ff2dddd9904a79850c2eae712ca323892
seokam36 <seokam36@gmail.com>
2026-08-23 14:23:30 +0900

    run()구현

--- src/screen2/view/KitchenProductView1.java
+++ src/screen2/view/KitchenProductView1.java
@@ -15,7 +15,26 @@ public class KitchenProductView1 {
     Scanner sc = new Scanner(System.in);
     public void run(){
         while (true){
-            System.out.println("만들 음식 선택 : 1.");
+                System.out.println("1.음식 조리 2.재고 확인 3.종료");
+                System.out.println(">> 선택 : ");
+                String ch = sc.next();
+            switch (ch){
+                case "1":
+                    cook();
+                    break;
+                case "2":
+                    // 재고확인
+                    break;
+                case "3":
+                    System.out.println("종료합니다.");
+                    return;
+                default:
+                    System.out.println("잘못된 입력입니다(1~3)");
+            }
+
         }
+    }
+
+    private void cook() {
     }
 }
