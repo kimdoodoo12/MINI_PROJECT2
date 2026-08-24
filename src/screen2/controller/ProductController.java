666490df6e9fd94b4048ae9ce6ffdd40b4414c43
abjsky1 <abjsky1@gmail.com>
2026-08-24 14:56:31 +0900

    1차_취합용

--- src/screen2/controller/ProductController.java
+++ src/screen2/controller/ProductController.java
@@ -1,5 +1,24 @@
 package screen2.controller;
 
+import screen2.model.dao.ProductDAO;
+import screen2.model.dto.ProductDTO;
+import screen2.view.ProductView;
+
 public class ProductController{
     
+    private ProductController(){};
+    private static final ProductController instance = new ProductController();
+    public static ProductController getInstance(){return instance;}
+
+    private ProductDAO pd = ProductDAO.getInstance();
+
+
+    public boolean addProductLog_order(ProductDTO productDTO){
+
+        boolean result = pd.addProductLog_order(productDTO);
+
+
+        return true;
+    }
+
 }
--- src/screen2/model/dto/ProductDTO.java
+++ src/screen2/model/dto/ProductDTO.java
@@ -2,4 +2,31 @@ package screen2.model.dto;
 
 public class ProductDTO {
 
+    int 재료번호;
+    int 수량;
+
+    public ProductDTO(int 재료번호, int 수량) {
+        this.재료번호 = 재료번호;
+        this.수량 = 수량;
+    }
+
+    public int get재료번호() {
+        return 재료번호;
+    }
+    public void set재료번호(int 재료번호) {
+        this.재료번호 = 재료번호;
+    }
+    public int get수량() {
+        return 수량;
+    }
+    public void set수량(int 수량) {
+        this.수량 = 수량;
+    }
+
+    @Override
+    public String toString() {
+        return "ProductDTO [재료번호=" + 재료번호 + ", 수량=" + 수량 + "]";
+    }
+
+    
 }
