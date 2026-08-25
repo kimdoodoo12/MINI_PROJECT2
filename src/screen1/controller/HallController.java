96f1aece99b9ccb8c5d45295ec671963fb69b4cc
kimdoodoo12 <kimdoodoo12@gmail.com>
2026-08-25 19:44:09 +0900

    7:44PM

--- src/screen1/controller/HallController.java
+++ src/screen1/controller/HallController.java
@@ -230,103 +230,10 @@ public class HallController {
     }
 
     public void changeGameState(){
-
+        
     }
 
     public void minusGold(){
         
     }
 }
-
-// public class CallCenterMain {
-
-// public static void main(String[] args) {
-
-// System.out.println("=== 고객센터 시스템 시작 ===");
-
-// // [1] 상담원 3명 스레드 풀 생성 및 대기 상태 초기화
-
-// ThreadPoolExecutor agentPool = (ThreadPoolExecutor)
-// Executors.newFixedThreadPool(3);
-
-// // [2] 전화 인입 스레드 생성 (익명 구현체)
-
-// Runnable customerProducer = new Runnable() {
-
-// @Override
-
-// public void run() {
-
-// for (int customerId = 1; customerId <= 20; customerId++) {
-
-// try {
-
-// Thread.sleep(3000); // 3초 간격 전화 인입
-
-// } catch (InterruptedException e) {}
-
-// // [3] CallTask 구현체 객체 생성 및 스레드풀에 배정
-
-// CallTask task = new CallTask(customerId);
-
-// agentPool.submit(task);
-
-// // [4] 현재 풀 상태 계산
-
-// int activeAgents = agentPool.getActiveCount();
-
-// int idleAgents = agentPool.getCorePoolSize() - activeAgents;
-
-// int waitingCustomers = agentPool.getQueue().size();
-
-// System.out.println("\n==[현황] 고객 " + customerId + "번 인입 , [대기 상담원]: " +
-// idleAgents + "명 , [통화
-// 중]: " + activeAgents + "명 , [대기 고객]: " + waitingCustomers + "명\n");
-
-// } // for end
-
-// // 100건 인입 완료 후 스레드 풀 종료 예약
-
-// agentPool.shutdown();
-
-// } // run end
-
-// }; // 익명구현체 end
-
-// // [5] 전화 인입 스레드 시작
-
-// Thread producerThread = new Thread(customerProducer);
-
-// producerThread.start();
-
-// }
-
-// }
-
-// class CallTask implements Runnable {
-
-// private Random random = new Random();
-
-// private int customerId;
-
-// public CallTask( int customerId ){ this.customerId = customerId; }
-
-// @Override
-
-// public void run() {
-
-// String agentName = Thread.currentThread().getName() ;
-
-// System.out.println(agentName+"-상담원 고객통화 시작-" + customerId);
-
-// try {
-
-// Thread.sleep(6000 + random.nextInt(6001)); // 6000ms ~ 12000ms (6초 ~ 12초)
-
-// } catch (InterruptedException e) {}
-
-// System.out.println(agentName+"-상담원 고객통화 종료-" + customerId);
-
-// }
-
-// }
