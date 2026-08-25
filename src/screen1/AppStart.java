1126d945ea20da0f91cd33bf6859dfabbe167606
kimdoodoo12 <kimdoodoo12@gmail.com>
2026-08-25 10:02:18 +0900

    게임제한시간 기능작업중

--- src/screen1/AppStart.java
+++ src/screen1/AppStart.java
@@ -6,10 +6,10 @@ import screen1.view.TotalStatusView;
 
 public class AppStart {
     public static void main(String[] args) {
-        // HallGameView2.getInstance().run();
+        HallGameView2.getInstance().run();
         // StatusView test = StatusView.getinstance();
         // test.printDailySatus();
-        TotalStatusView test2 = TotalStatusView.getinstance();
-        test2.printFinalSatus();
+        // TotalStatusView test2 = TotalStatusView.getinstance();
+        // test2.printFinalSatus();
     }
 }
