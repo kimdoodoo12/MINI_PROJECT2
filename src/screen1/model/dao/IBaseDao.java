0b580de2bc0655fa5b2e233d31298011bdf4e685
kimdoodoo12 <kimdoodoo12@gmail.com>
2026-08-27 17:49:54 +0900

    로컬DB로 변경

--- src/screen1/model/dao/IBaseDao.java
+++ src/screen1/model/dao/IBaseDao.java
@@ -7,6 +7,10 @@ import java.util.Properties;
 import db.DBConfig;
 
 public class IBaseDao {
+
+    private String url = "jdbc:mysql://127.0.0.1:3306/MINI2";
+    private String user = "root";
+    private String pwd = "1234";
     // 2. 연동 인터페이스 , protected : 상속 관계이면 다른 패키지도 접근 허용
     protected Connection conn;
     // 3. 연동 메소드
@@ -15,8 +19,9 @@ public class IBaseDao {
             // 3-1: mysql Driver 클래스 로드 한다
             Class.forName("com.mysql.cj.jdbc.Driver");
             // 3-2: db.properties에서 연동 정보 로드 후 서버와 연동, 성공하면 conn(인터페이스) 대입
-            Properties prop = DBConfig.load();
-            conn = DriverManager.getConnection(prop.getProperty("url"), prop.getProperty("user"), prop.getProperty("pwd"));
+            // Properties prop = DBConfig.load();
+            // conn = DriverManager.getConnection(prop.getProperty("url"), prop.getProperty("user"), prop.getProperty("pwd"));
+            conn = DriverManager.getConnection(url, user, pwd);
         }catch(Exception e){System.out.println(e);}
 
     }
